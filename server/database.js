071059dc1f8ac70e318cff5173a5752366502096
TeoMili <teomili@hotmail.com>
2024-04-01 12:49:02 +0100

    retrieve query data

--- server/database.js
+++ server/database.js
@@ -15,25 +15,6 @@ client
     .connect()
     .then(() => {
         console.log('Connected to database');
-        
-        //execute required query
-        var query_result = client.query('SELECT * FROM projects', (err, result) => {
-            if(err){
-                console.log('Error executing query', err);
-            } else{
-                console.log('Query result: ', result.rows);
-            }
-            
-            //close the connection
-           // client
-            //.end()
-            //.then(() => {
-              //  console.log('Connection closed');
-            //})
-            //.catch((err) => {
-              //  console.error('Error closing connection', err);     
-           // });
-        });
     })
     .catch((err) => {
         console.error('Error connecting to the database', err);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 //create a server
 const http = require('http');
-const db = require('./database');
+const client = require('./database');
 
 const server = http.createServer(function(req, res) {
     //set CORS headers
@@ -10,8 +10,19 @@ const server = http.createServer(function(req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hello World');
-    res.write(db.query_result.rows);
-    res.end();
+
+    //get data from database
+    var queryInfo = 'SELECT * FROM projects';
+    client.query(queryInfo, (err, result) => {
+        if(err){
+            console.log('Error executing query', err);
+        } else{
+            console.log("Query result: ", result.rows);
+            res.write(JSON.stringify(result.rows));
+            res.end();
+        }
+    });
+    
 });
 server.listen(5000, function(){
     console.log('Listening on port 5000');
