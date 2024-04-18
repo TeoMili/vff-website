90e94340adf8be838ffa4dd1d537a9210430ee0f
TeoMili <teomili@hotmail.com>
2024-04-18 10:41:01 +0100

    added form elements

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 function Contact(){
+    const [textarea, setTextarea] = useState("");
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        alert("Submited");
+    }
+
     return(
+        <>
         <h1> This is the contact page </h1>
+        <form>
+            <label> Nume: 
+                <input type="text" />
+            </label>
+            <br />
+            <label> Email: 
+                <input type="text" />
+            </label>
+            <textarea/>
+            <br />
+            <input type="submit" />
+        </form>
+        </>
     )
 }
 
