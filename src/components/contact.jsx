8be2888c68164cffe8d12b9784d64acfd8268862
TeoMili <teomili@hotmail.com>
2024-04-19 12:57:18 +0100

    edited form components

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
 function Contact(){
-    const [textarea, setTextarea] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleTextareaChange = (event) => {
+        setMessage(event.target.value);
+    };
 
     const handleSubmit = (event) =>{
         event.preventDefault();
+        //check if fields are empty
+        if(name == "" || email == "" || message == ""){
+            alert("Please complete all fields");
+            return;
+        }
+        console.log(name, email, message);
         alert("Submited");
+        
     }
 
     return(
@@ -12,15 +33,15 @@ function Contact(){
         <h1> This is the contact page </h1>
         <form>
             <label> Nume: 
-                <input type="text" />
+                <input type="text" onChange={handleNameChange}/>
             </label>
             <br />
             <label> Email: 
-                <input type="text" />
+                <input type="text" onChange={handleEmailChange}/>
             </label>
-            <textarea/>
+            <textarea value={message} onChange={handleTextareaChange}/>
             <br />
-            <input type="submit" />
+            <button type="submit" onClick={handleSubmit}> Submit </button>
         </form>
         </>
     )
